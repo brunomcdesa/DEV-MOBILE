d0fac826a0a71e11f9cba92af9ec540ae1e17d09
brunomcdesa <102191355+brunomcdesa@users.noreply.github.com>
2022-09-28 15:30:26 -0300

    Update index.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,18 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import {Feather} from '@expo/vector-icons';
 
 export default function Header({nome, endereco}){
     return(
         <View style={estilos.container}>
-             <Text>{nome}</Text>
-             <Text>{endereco}</Text>
+            <View style = {estilos.botaoUser}>
+                <TouchableOpacity >
+                    <Feather  name="user" size={27}/>
+                </TouchableOpacity>
+            </View>
+            <View style={estilos.textosDirecao}>
+                 <Text style={estilos.textoNome}>{nome}</Text>
+                <Text style={estilos.textoEndereco}>{endereco}</Text>
+             </View>
         </View>
     )
 }
@@ -16,5 +24,25 @@ const estilos = StyleSheet.create({
         paddingStart: 16,
         paddingEnd: 16,
         paddingBottom: 44,
-    }
+    },
+    textosDirecao:{
+        flexDirection: "column",
+    },
+    textoNome:{
+        fontWeight: "bold",
+        fontSize: 35,
+        marginTop: 20
+    },
+    textoEndereco:{
+        fontSize: 20,
+    },
+    botaoUser:{
+        marginTop: 20,
+        width: 65,
+        height: 65,
+        backgroundColor: "#rgba(255, 255, 255, 0.5)",
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 50,
+    },
 })
