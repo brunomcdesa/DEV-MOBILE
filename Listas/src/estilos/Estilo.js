b3d6b03270c125e5b82f586aed5b330def95e814
brunomcdesa <102191355+brunomcdesa@users.noreply.github.com>
2022-10-19 21:35:55 -0300

    Create Estilo.js

---
+++ Listas/src/estilos/Estilo.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      itemDesc: {
+        fontSize: 22,
+      },
+      itemTitle:{
+          fontWeight: 'bold',
+          fontSize: 35
+      }
+})
+
